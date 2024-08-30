1de0b2b42c960c338d36dc2f4b7dc5a9886703a5
ibrahimabdalrhman <ibrahimabdalr7man@gmail.com>
2024-08-30 15:48:50 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Ensure to specify the database type
+      host: process.env.PG_HOST,
+      port: +process.env.PG_PORT,
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    TodoModule,
+  ],
+})
+export class AppModule {}
